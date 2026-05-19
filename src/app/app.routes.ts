7d6e449cca6f3115a6bd098f26469be885ba03c1
kaverikamble <kaverikamble1805@gmail.com>
2026-05-19 18:13:42 +0530

    implemented code for preloader,audio,backtotop,slider on bacground,social icons

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,60 +1,54 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./pages/header/header').then((m) => m.Header),
+  },
 
-//   {
-//     path: '',
-//     loadComponent: () =>
-//       import('./pages/home/home.component')
-//         .then(m => m.HomeComponent)
-//   },
+  //   {
+  //     path: 'about-us',
+  //     loadComponent: () =>
+  //       import('./pages/about-us/about-us.component')
+  //         .then(m => m.AboutUsComponent)
+  //   },
 
-//   {
-//     path: 'about-us',
-//     loadComponent: () =>
-//       import('./pages/about-us/about-us.component')
-//         .then(m => m.AboutUsComponent)
-//   },
+  //   {
+  //     path: 'contact-us',
+  //     loadComponent: () =>
+  //       import('./pages/contact-us/contact-us.component')
+  //         .then(m => m.ContactUsComponent)
+  //   },
 
-//   {
-//     path: 'contact-us',
-//     loadComponent: () =>
-//       import('./pages/contact-us/contact-us.component')
-//         .then(m => m.ContactUsComponent)
-//   },
+  //   {
+  //     path: 'events',
+  //     loadComponent: () =>
+  //       import('./pages/events/events.component')
+  //         .then(m => m.EventsComponent)
+  //   },
 
-//   {
-//     path: 'events',
-//     loadComponent: () =>
-//       import('./pages/events/events.component')
-//         .then(m => m.EventsComponent)
-//   },
+  //   {
+  //     path: 'services',
+  //     loadComponent: () =>
+  //       import('./pages/services/services.component')
+  //         .then(m => m.ServicesComponent)
+  //   },
 
-//   {
-//     path: 'services',
-//     loadComponent: () =>
-//       import('./pages/services/services.component')
-//         .then(m => m.ServicesComponent)
-//   },
-
-//   {
-//     path: 'blogs',
-//     loadComponent: () =>
-//       import('./pages/blogs/blogs.component')
-//         .then(m => m.BlogsComponent)
-//   },
+  //   {
+  //     path: 'blogs',
+  //     loadComponent: () =>
+  //       import('./pages/blogs/blogs.component')
+  //         .then(m => m.BlogsComponent)
+  //   },
 
   {
     path: 'footer',
-    loadComponent: () =>
-      import('./footer/footer/footer')
-        .then(m => m.Footer)
+    loadComponent: () => import('./footer/footer/footer').then((m) => m.Footer),
   },
 
   // 404 Page
   {
     path: '**',
-    redirectTo: ''
-  }
-
+    redirectTo: '',
+  },
 ];
